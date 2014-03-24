c4b6e5fb850e8fb3f7f7f68561b46f0b786b841e
lifeloverxg <yijunxiao88@gmail.com>
2014-03-24 16:51:14 -0400

    +
    +

---
+++ yijunxiao_for_test_only/wangjimima.php
@@ -0,0 +1,6 @@
+<form id="form1" name="form1" method="post" action="mailto.php" onSubmit="return CheckForm()">
+<input name="username" type="text" class="in" id="username" size="30" onmouseover="fEvent('mouseover',this)" onfocus="fEvent('focus',this)" onblur="fEvent('blur',this)" onmouseout="fEvent('mouseout',this)"  />
+
+<input type="submit" name="Submit" style="background:url(images/bo2.jpg); height:36px; width:113px; border:none; font-size:14px; font-weight:bold; color:#FFFFFF" value=" 确定找回 " />
+
+</form>
